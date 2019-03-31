2011e0b4af73f36198734cc883d7ceb0401cae95
kayyal <amir.kayyal@gmail.com>
2019-03-31 21:31:24 +0430

    first commit !

--- q04/NewspaperSubscription.java
+++ q04/NewspaperSubscription.java
@@ -1,6 +1,23 @@
 package assignment05.q04;
 
 public abstract class NewspaperSubscription {
-    
+    private String name ;
+    private int address;
+    private int rate;
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAddress() {
+        return address;
+    }
+
+    public int getRate() {
+        return rate;
+    }
 }
