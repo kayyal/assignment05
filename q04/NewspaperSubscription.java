3fb6bc963365ee87ddf066ffbe92ff5ad5a58692
kayyal <amir.kayyal@gmail.com>
2019-03-31 21:35:19 +0430

    add setter for address.

--- q04/NewspaperSubscription.java
+++ q04/NewspaperSubscription.java
@@ -1,21 +1,23 @@
 package assignment05.q04;
 
 public abstract class NewspaperSubscription {
-    private String name ;
+    private String name;
     private int address;
     private int rate;
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public int getAddress() {
         return address;
     }
+
+    public abstract void setAddress(int address);
 
     public int getRate() {
         return rate;
